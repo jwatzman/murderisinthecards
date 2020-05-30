a8a250fb41bad0a7bf18f3acd8860250b38657dc
Josh Watzman <github@jwatzman.org>
2020-05-31 00:05:47 +0100

    Send a message

--- murderisinthecards-client/src/App.tsx
+++ murderisinthecards-client/src/App.tsx
@@ -2,6 +2,7 @@ import * as Colyseus from "colyseus.js";
 import React from 'react';
 
 const MessageContext = React.createContext<string[]>([]);
+const SendMessageContext = React.createContext<(m:string) => void>(undefined!);
 
 function App() {
 	const [room, setRoom] = React.useState<Colyseus.Room | null>(null);
@@ -32,10 +33,46 @@ function App() {
 		return <div>Connecting...</div>;
 	}
 
+	const sendMessage = (message: string) => room.send(
+		'sendtestchat',
+		message
+	);
+
 	return (
 		<MessageContext.Provider value={messages}>
-			<MessageList />
+			<SendMessageContext.Provider value={sendMessage}>
+				<MessageSend />
+				<MessageList />
+			</SendMessageContext.Provider>
 		</MessageContext.Provider>
+	);
+}
+
+function MessageSend() {
+	const [message, setMessage] = React.useState('');
+	const sendMessage = React.useContext(SendMessageContext);
+
+	const handleSubmit = (evt: React.SyntheticEvent) => {
+		evt.preventDefault();
+		sendMessage(message);
+		setMessage('');
+	};
+
+	const handleChange = (evt: React.SyntheticEvent) => {
+		const target = evt.target as typeof evt.target & {
+			value: string;
+		};
+		setMessage(target.value);
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<label>
+				Message:
+				<input type="text" value={message} onChange={handleChange} />
+			</label>
+			<input type="submit" value="Send" />
+		</form>
 	);
 }
 
