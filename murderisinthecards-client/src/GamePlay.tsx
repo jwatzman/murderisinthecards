14b8ccbae3253fbcbbf42d3d33d9265a4291cfaa
Josh Watzman <github@jwatzman.org>
2020-06-13 20:56:16 +0100

    Controls to the right

--- murderisinthecards-client/src/GamePlay.tsx
+++ murderisinthecards-client/src/GamePlay.tsx
@@ -6,13 +6,19 @@ import GameMessages from './GameMessages';
 import TurnActions from './TurnActions';
 import TurnOrder from './TurnOrder';
 
+import Styles from './GamePlay.module.css';
+
 export default function GamePlay() {
 	return (
 		<>
-			<GameBoard />
-			<TurnOrder />
-			<DieRoll />
-			<TurnActions />
+			<div className={Styles.wrap}>
+				<GameBoard />
+				<div className={Styles.controls}>
+					<TurnOrder />
+					<DieRoll />
+					<TurnActions />
+				</div>
+			</div>
 			<GameMessages />
 		</>
 	);
