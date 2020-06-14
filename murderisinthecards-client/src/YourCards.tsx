7c11452121814b6240c52798f8505bbe537e950a
Josh Watzman <github@jwatzman.org>
2020-06-14 23:38:10 +0100

    Display cards better

--- murderisinthecards-client/src/YourCards.tsx
+++ murderisinthecards-client/src/YourCards.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 
+import { Room, Suspect, Weapon } from 'murderisinthecards-common/Consts';
+
 import { YourCardsContext } from './Context';
 
 export default function YourCards() {
 	const cards = React.useContext(YourCardsContext);
+
+	const suspects = [];
+	const weapons = [];
+	const rooms = [];
+
+	for (const card of cards) {
+		if (Object.values(Suspect).includes(card as Suspect)) {
+			suspects.push(card);
+		} else if (Object.values(Weapon).includes(card as Weapon)) {
+			weapons.push(card);
+		} else if (Object.values(Room).includes(card as Room)) {
+			rooms.push(card);
+		}
+	}
+
 	return (
 		<div>
-			Your cards: {cards.join(', ')}
+			Your cards:
+			<ul>
+				<li>{suspects.join(', ')}</li>
+				<li>{weapons.join(', ')}</li>
+				<li>{rooms.join(', ')}</li>
+			</ul>
 		</div>
 	);
 }
