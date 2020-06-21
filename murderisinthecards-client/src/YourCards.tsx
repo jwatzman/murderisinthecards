d1cfec2eb9a3abee13ab3668f72ccc355a37f8f3
Josh Watzman <github@jwatzman.org>
2020-06-21 22:50:52 +0100

    Don't display bullet point if no cards

--- murderisinthecards-client/src/YourCards.tsx
+++ murderisinthecards-client/src/YourCards.tsx
@@ -10,36 +10,35 @@ import {
 import { YourCardsContext } from './Context';
 
 export default function YourCards() {
+	return (
+		<div>
+			Your cards:
+			<ul>
+				<CardsLine src={Object.values(Suspect)} />
+				<CardsLine src={Object.values(Weapon)} />
+				<CardsLine src={Object.values(Room)} />
+			</ul>
+		</div>
+	);
+}
+
+function CardsLine({ src }: { src: Card[] }) {
 	const cards = React.useContext(YourCardsContext);
-
-	const suspects: Suspect[] = [];
-	const weapons: Weapon[] = [];
-	const rooms: Room[] = [];
-
-	const checkAndPush = <T extends Card>(src: T[], target: T[]) => {
-		for (const elem of src) {
-			if (cards.includes(elem)) {
-				target.push(elem);
-			}
-		}
-	};
 
 	// This is a somewhat inefficient way to deal with this, but it's simple,
 	// preserves the types, and keeps the cards displayed in the UI in a
 	// consistent order. There aren't enough cards in the game for the
 	// inefficiency to really matter.
-	checkAndPush(Object.values(Suspect), suspects);
-	checkAndPush(Object.values(Weapon), weapons);
-	checkAndPush(Object.values(Room), rooms);
+	const filtered: Card[] = [];
+	for (const elem of src) {
+		if (cards.includes(elem)) {
+			filtered.push(elem);
+		}
+	}
 
-	return (
-		<div>
-			Your cards:
-			<ul>
-				<li>{suspects.join(', ')}</li>
-				<li>{weapons.join(', ')}</li>
-				<li>{rooms.join(', ')}</li>
-			</ul>
-		</div>
-	);
+	if (filtered.length > 0) {
+		return <li>{filtered.join(', ')}</li>;
+	} else {
+		return null;
+	}
 }
